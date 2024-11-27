864b81abafe23480f5bc3a0c113a9244e4b3d60d
minzunim <greenteapig6@gmail.com>
2024-11-27 22:57:32 +0900

    [Fix] 로그인 에러 처리 수정

--- src/contexts/Authcontext.tsx
+++ src/contexts/Authcontext.tsx
@@ -3,57 +3,56 @@ import { useNavigate } from "react-router-dom";
 import { supabase } from "../config/supabase";
 
 interface User {
-  email: string;
-  email_verified: boolean;
-  phone_verified: boolean;
-  sub: string;
+    email: string;
+    email_verified: boolean;
+    phone_verified: boolean;
+    sub: string;
 }
 
 interface UserContextType {
-  userData: User;
-  logout: () => void;
+    userData: User;
+    logout: () => void;
 }
 
 export const UserContext = createContext<UserContextType | null>(null);
 
 const UserProvider = ({ children }: any) => {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
-  const navigate = useNavigate();
+    const [isLogin, setIsLogin] = useState<boolean>(false);
+    const navigate = useNavigate();
 
-  let userData: User = {
-    email: "",
-    email_verified: false,
-    phone_verified: false,
-    sub: "",
-  };
+    let userData: User = {
+        email: "",
+        email_verified: false,
+        phone_verified: false,
+        sub: "",
+    };
 
-  // 로컬 스토리지 확인
-  const authData = localStorage.getItem(process.env.REACT_APP_SUPABASE_TOKEN!);
+    // 로컬 스토리지 확인
+    const authData = localStorage.getItem(process.env.REACT_APP_SUPABASE_TOKEN!);
 
-  useEffect(() => {
-    if (!authData) {
-      //setIsLogin(false);
-      alert("로그인이 필요합니다.");
-      navigate("/login");
-    } else {
-      //setIsLogin(true);
-      userData = JSON.parse(authData).user_metadata;
-    }
-  }, []);
+    useEffect(() => {
+        if (!authData) {
+            //setIsLogin(false);
+            navigate("/login");
+        } else {
+            //setIsLogin(true);
+            userData = JSON.parse(authData).user_metadata;
+        }
+    }, []);
 
-  // 로그아웃
-  const logout = () => {
-    localStorage.removeItem(process.env.REACT_APP_SUPABASE_TOKEN!);
-    alert("로그아웃 완료");
-    navigate("/login");
-    return;
-  };
+    // 로그아웃
+    const logout = () => {
+        localStorage.removeItem(process.env.REACT_APP_SUPABASE_TOKEN!);
+        alert("로그아웃 완료");
+        navigate("/login");
+        return;
+    };
 
-  return (
-    <UserContext.Provider value={{ userData, logout }}>
-      {children}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={{ userData, logout }}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 export default UserProvider;
